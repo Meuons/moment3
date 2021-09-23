fd26273d592a1df581389391f89fdc2c21f00d3c
Meuons <89832844+Meuons@users.noreply.github.com>
2021-09-23 12:54:25 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,24 @@ const {src, dest, watch, series, parallel} = require("gulp");
 const concat = require('gulp-concat');
 const minifyCSS = require('gulp-minify-css');
 const terser = require('gulp-terser');
-const livereload = require('gulp-livereload');
-let browserSync = require('browser-sync').create();
+var browserSync = require('browser-sync').create();
+const sass = require('gulp-sass')(require('sass'));
+sass.compiler = require('node-sass');
+
+
 const options = {
     html: {
         removeAttributeQuotes: false,
         removeOptionalTags: false,
     },
 };
+
+const gulp = require('gulp');
+const cssScss = require('gulp-css-scss');
+
+
+
+
 
 function reloadTask() {
 browserSync.init({
@@ -18,19 +28,26 @@ browserSync.init({
 }
 
 
-livereload({ start: true })
 //Filepaths
 const files = { 
     htmlPath: "src/**/*.html",
-    cssPath: "src/css/*.css",
     jsPath: "src/**/*.js",
-    imgPath: "src/**/*.jpg"
+    imgPath: "src/**/*.jpg",
+    sassPath: "src/**/*.scss"
 }
 
+function sassTask() {
+    return src(files.sassPath)
+    .pipe(concat('styles.scss'))
+        .pipe(sass().on("error", sass.logError))
+        .pipe(minifyCSS())
+        .pipe(dest("pub/css"))
+        .pipe(browserSync.stream());
+}
+        
 //HTML-task, duplicate files
 function htmlTask() {
 return src(files.htmlPath)
-
 .pipe(dest('pub'))
 .pipe(browserSync.stream());
 
@@ -38,20 +55,10 @@ return src(files.htmlPath)
 //Img-task, duplicate files
 function imgTask() {
     return src(files.imgPath)
-    
     .pipe(dest('pub'))
     .pipe(browserSync.stream());
-    
     }
-//CSS-task, minify, concat
-function cssTask() {
-    return src(files.cssPath)
-    .pipe(concat('styles.css'))
-    .pipe(minifyCSS())
-    .pipe(dest('pub/css'))
-    .pipe(browserSync.stream());
-    
-}
+
 //JS-task,  minify, concat
 function jsTask() {
     return src(files.jsPath)
@@ -60,13 +67,14 @@ function jsTask() {
     .pipe(dest('pub/js'))
     .pipe(browserSync.stream());
 }
+
 //Watch
+
 function watchTask(){
-  watch([files.htmlPath, files.jsPath, files.cssPath, files.imgPath], parallel(htmlTask, jsTask, cssTask, imgTask, reloadTask)).on("change", browserSync.reload);
+  watch([files.htmlPath, files.jsPath, files.imgPath, files.sassPath], parallel(htmlTask, jsTask, imgTask, sassTask, reloadTask)).on("change", browserSync.reload);
 }
 
-
 exports.default = series (
-    parallel(htmlTask, jsTask, imgTask, cssTask, reloadTask),
+    parallel(htmlTask, jsTask, imgTask, sassTask, reloadTask),
     watchTask
 );
